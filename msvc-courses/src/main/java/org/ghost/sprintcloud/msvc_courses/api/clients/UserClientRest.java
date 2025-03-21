bf9a98f357c23f9266e0afe2907425428505d8a2
Alejandro Giraldo Serna <giraldosernaalejandro16@gmail.com>
2025-03-21 18:03:53 -0500

    DBs Dockerizado

--- msvc-courses/src/main/java/org/ghost/sprintcloud/msvc_courses/api/clients/UserClientRest.java
+++ msvc-courses/src/main/java/org/ghost/sprintcloud/msvc_courses/api/clients/UserClientRest.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "msvc-users", url = "localhost:8001")
+@FeignClient(name = "msvc-users", url = "msvc-users:8001")
 public interface UserClientRest {
     @GetMapping("/{id}")
     User getUser(@PathVariable Long id);
